fdafd583d02b8d64cd5c5f958dad56e62af34ba1
ItzTintu <94976219+ItzTintu@users.noreply.github.com>
2021-11-29 10:50:04 +0530

    Update tkinter 12.py
    Fixed Softlock

--- Python Code/tkinter 12.py
+++ Python Code/tkinter 12.py
@@ -1,6 +1,6 @@
 from ctypes import cast, POINTER
 from comtypes import CLSCTX_ALL
-from pycaw.pycaw import AudioUtilities, IAudioEndpointVolume
+#from pycaw.pycaw import AudioUtilities, IAudioEndpointVolume
 import math
 import pygame
 import screen_brightness_control as sbc
@@ -48,6 +48,7 @@ def work():
         Obj1=PhotoImage(file='DeidMann.png') 
         Obj2=PhotoImage(file='BurningCar.png')
         Obj3=PhotoImage(file='BloodyWrench.png')
+        Wit1=PhotoImage(file='Witness.png')
         tutorial_file = open(r"MainTutorial.txt" , 'r')
         tutorial_dialogue=tutorial_file.read() 
         tutorial_list=tutorial_dialogue.split('\n\n') 
@@ -56,7 +57,7 @@ def work():
         for i in range(len(tutorial_list)):
                        tup1=tutorial_list[i].partition(': ')
                        l1.append(tup1[0]) 
-                       l2.append(tup1[-1]) 
+                       l2.append(tup1[-1])
         event_file = open(r"EventText.txt" , 'r')
         event_dialogue=event_file.read()
         event_list=event_dialogue.split('\n\n')
@@ -66,29 +67,39 @@ def work():
                        tup2=event_list[i].partition(': ')
                        l3.append(tup2[0])
                        l4.append(tup2[-1])
+        wit_file = open(r"WitnessText.txt" , 'r')
+        wit_dialogue=wit_file.read()
+        wit_list=wit_dialogue.split('\n\n')
+        l5=[]
+        l6=[]
+        for i in range(len(wit_list)):
+                       tup3=wit_list[i].partition(': ')
+                       l5.append(tup3[0])
+                       l6.append(tup3[-1])
         SceneScreen= Canvas(GameWindow, width=600, height=500)
         SceneScreen.create_image(300,200,image=SceneImage)
         SceneScreen.create_image(300,400,image=TextFrame) 
         SceneScreen.create_image(300,10,image=ObjectiveFrame)
         SceneScreen.create_image(310,200,image=Obj1, tags="Body")
         SceneScreen.create_image(510,100,image=Obj2, tags="Car")
-        #SceneScreen.create_image(210,250,image=Obj3, tags="Wrench") 
+        #SceneScreen.create_image(210,250,image=Obj3, tags="Wrench")
+        SceneScreen.create_image(100, 200, image=Wit1)
         SceneScreen.pack()
         LineButton=ttk.Button(SceneScreen, text='     >     ')
         ObjectiveBar=Label(SceneScreen, text='', font=GameFont, foreground='white', background='black', wraplength=200)
-        def hovertrue(): 
-            if TextBox.instate(['disabled'])==True:
+        def hovertrueobj(): 
+            if TextBox.instate(['disabled'])==True and LineButton.instate(['disabled'])==True:
                 ObjectiveBar['text']='[Examine]'
-        def hoverfalse(): #And vice-versa
-            if TextBox.instate(['disabled'])==True:
+        def hoverfalseobj():
+            if TextBox.instate(['disabled'])==True and LineButton.instate(['disabled'])==True:
                 ObjectiveBar['text']=''
-        count=1
-        def clicktrue(n, Obj_id):
-            nonlocal count
-            SceneScreen.delete(Obj_id)
-            SceneScreen.pack()
-            if TextBox.instate(['disabled'])==True:
-                if count<(len(l4)):
+        countobj=1
+        def clicktrueobj(n, Obj_id):
+            if TextBox.instate(['disabled'])==True and LineButton.instate(['disabled'])==True:
+                nonlocal countobj
+                SceneScreen.delete(Obj_id)
+                SceneScreen.pack()
+                if countobj<(len(l4)):
                     str1=l4[n]
                     SpeakBox['text']=l3[n]
                     TextBox['text']=''
@@ -101,9 +112,9 @@ def work():
                         SceneScreen.create_window(300,400, window=TextBox)
                         SceneScreen.update()
                         SceneScreen.pack(side='bottom')
-                        time.sleep(0.03)
+                        time.sleep(0.04)
                     SceneScreen['state']='normal'
-                elif count==(len(l4)):
+                elif countobj==(len(l4)):
                     str1=l4[n]
                     SpeakBox['text']=l3[n]
                     TextBox['text']=''
@@ -120,19 +131,65 @@ def work():
                     SceneScreen['state']='normal'
                     LineButton['state']='active'
                     TextBox['state']='active'
-            count+=1            
+                countobj+=1
+        def hovertruewit():
+            if TextBox.instate(['disabled']) and LineButton.instate(['disabled']) and countobj>(len(l4)):
+                ObjectiveBar['text']='[Interrogate]'
+        def hoverfalsewit():
+           if TextBox.instate(['disabled']) and LineButton.instate(['disabled']) and countobj>(len(l4)):
+                ObjectiveBar['text']=''
+        #def clicktruebut(n, But_id):    
+        countwit=1
+        def clicktruewit(n, Wit_id):
+            if TextBox.instate(['disabled']) and LineButton.instate(['disabled']) and countobj>(len(l4)):
+                nonlocal countwit
+                SceneScreen.delete(Wit_id)
+                SceneScreen.pack()
+                l5.reverse()
+                l6.reverse()
+                for i in l6:
+                    if i[0]==str(n):
+                        i=i[1:]
+                        l2[k:k]=[i]
+                for j in l5:
+                    if j[0]==str(n):
+                        j=j[1:]
+                        l1[m:m]=[j]
+                l6.reverse()
+                l5.reverse()
+                str1=l2[k]
+                SpeakBox['text']=l1[m]
+                TextBox['text']=''
+                for j in str1:
+                    SceneScreen['state']='disabled'
+                    ObjectiveBar['text']='[Read]'
+                    TextBox['text']+=j
+                    TextBox.update()
+                    TextBox.pack(side='left')
+                    SceneScreen.create_window(300,400, window=TextBox)
+                    SceneScreen.update()
+                    SceneScreen.pack(side='bottom')
+                    time.sleep(0.04)
+                SceneScreen['state']='normal'
+                LineButton['state']='active'
+                TextBox['state']='active'
+                countwit+=1   
         Obj1_id = SceneScreen.create_image(310,200,image=Obj1, tags="Body")
-        SceneScreen.tag_bind(Obj1_id, "<Enter>", lambda p: hovertrue())
-        SceneScreen.tag_bind(Obj1_id, "<Leave>", lambda p: hoverfalse())
-        SceneScreen.tag_bind(Obj1_id, "<Button-1>", lambda p: clicktrue(0,Obj1_id))
+        SceneScreen.tag_bind(Obj1_id, "<Enter>", lambda p: hovertrueobj())
+        SceneScreen.tag_bind(Obj1_id, "<Leave>", lambda p: hoverfalseobj())
+        SceneScreen.tag_bind(Obj1_id, "<Button-1>", lambda p: clicktrueobj(0,Obj1_id))
         Obj2_id = SceneScreen.create_image(510,100,image=Obj2, tags="Car")
-        SceneScreen.tag_bind(Obj2_id, "<Enter>", lambda p: hovertrue())
-        SceneScreen.tag_bind(Obj2_id, "<Leave>", lambda p: hoverfalse())
-        SceneScreen.tag_bind(Obj2_id, "<Button-1>", lambda p: clicktrue(1,Obj2_id))
-        Obj3_id = SceneScreen.create_image(210,250,image=Obj3, tags="Wrench")
-        SceneScreen.tag_bind(Obj3_id, "<Enter>", lambda p: hovertrue())
-        SceneScreen.tag_bind(Obj3_id, "<Leave>", lambda p: hoverfalse())
-        SceneScreen.tag_bind(Obj3_id, "<Button-1>", lambda p: clicktrue(2,Obj3_id))
+        SceneScreen.tag_bind(Obj2_id, "<Enter>", lambda p: hovertrueobj())
+        SceneScreen.tag_bind(Obj2_id, "<Leave>", lambda p: hoverfalseobj())
+        SceneScreen.tag_bind(Obj2_id, "<Button-1>", lambda p: clicktrueobj(1,Obj2_id))
+        Obj3_id = SceneScreen.create_image(490,175,image=Obj3, tags="Wrench")
+        SceneScreen.tag_bind(Obj3_id, "<Enter>", lambda p: hovertrueobj())
+        SceneScreen.tag_bind(Obj3_id, "<Leave>", lambda p: hoverfalseobj())
+        SceneScreen.tag_bind(Obj3_id, "<Button-1>", lambda p: clicktrueobj(2,Obj3_id))
+        Wit1_id = SceneScreen.create_image(100, 200, image=Wit1)
+        SceneScreen.tag_bind(Wit1_id, "<Enter>", lambda p: hovertruewit())
+        SceneScreen.tag_bind(Wit1_id, "<Leave>", lambda p: hoverfalsewit())
+        SceneScreen.tag_bind(Wit1_id, "<Button-1>", lambda p: clicktruewit(0,Wit1_id))
         SceneScreen.pack(side='bottom')
         ObjectiveBar.pack(side='bottom')
         SceneScreen.create_window(300, 20, window=ObjectiveBar)
@@ -152,11 +209,11 @@ def work():
             k+=1
             m+=1
             if k<(len(l2)) and m<(len(l1)) :
-                str2=l2[k]
+                str1=l2[k]
                 SpeakBox['text']=l1[m]
                 TextBox['text']=''
-                for j in str2:
-                    if str2[-1]=='-':
+                for j in str1:
+                    if str1[-1]=='-':
                         TextBox['state']='disabled'
                         LineButton['state']='disabled'
                         break
@@ -168,7 +225,7 @@ def work():
                     SceneScreen.create_window(300,400, window=TextBox)
                     SceneScreen.update()
                     SceneScreen.pack(side='bottom')
-                    time.sleep(0.03)
+                    time.sleep(0.04)
                 else:
                     LineButton['state']='active'
         LineButton["command"]=next_line
@@ -198,20 +255,20 @@ def work():
         Button(frame2,text="Play Music",command=pyglet2.play_music).place(x=250,y=90)
         volume=Label(frame2,text="Volume")
         volume.place(x=30,y=130)
-        devices=AudioUtilities.GetSpeakers()
-        interface=devices.Activate(IAudioEndpointVolume._iid_,CLSCTX_ALL,None)
-        volume=cast(interface,POINTER(IAudioEndpointVolume))
+        #devices=AudioUtilities.GetSpeakers()
+        #interface=devices.Activate(IAudioEndpointVolume._iid_,CLSCTX_ALL,None)
+        #volume=cast(interface,POINTER(IAudioEndpointVolume))
         def redlevelvolume():
-            currentvolume=volume.GetMasterVolumeLevel()
-            volume.SetMasterVolumeLevel(currentvolume-6.0,None)
-        Button(frame2,text="Reduce Volume",command=redlevelvolume).place(x=250,y=130)
-        devices2=AudioUtilities.GetSpeakers()
-        interface2=devices2.Activate(IAudioEndpointVolume._iid_,CLSCTX_ALL,None)
-        volume2=cast(interface2,POINTER(IAudioEndpointVolume))
+            #currentvolume=volume.GetMasterVolumeLevel()
+            #volume.SetMasterVolumeLevel(currentvolume-6.0,None)
+        Button(frame2,text="Reduce Volume" ''', command=redlevelvolume''').place(x=250,y=130)
+        #devices2=AudioUtilities.GetSpeakers()
+        #interface2=devices2.Activate(IAudioEndpointVolume._iid_,CLSCTX_ALL,None)
+        #volume2=cast(interface2,POINTER(IAudioEndpointVolume))
         def inclevelvolume():
-            currentvolume2=volume2.GetMasterVolumeLevel()
-            volume2.SetMasterVolumeLevel(currentvolume2+6.0,None)
-        Button(frame2,text="Increase Volume",command=inclevelvolume).place(x=100,y=130)
+            #currentvolume2=volume2.GetMasterVolumeLevel()
+            #volume2.SetMasterVolumeLevel(currentvolume2+6.0,None)
+        Button(frame2,text="Increase Volume"''',command=inclevelvolume''').place(x=100,y=130)
         brightness=Label(frame2,text="Brightness")
         brightness.place(x=30,y=50)
         inputbrightness= Entry(frame2)
@@ -240,3 +297,5 @@ def work():
     canvas.create_image(0,0,image=background,anchor="nw")
     top.mainloop()
 work()
+
+
